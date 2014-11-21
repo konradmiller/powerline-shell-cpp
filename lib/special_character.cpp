8949e043a0efe07f214ba052e75dee33827534db
Konrad Miller <miller@kit.edu>
2014-11-21 23:19:08 +0100

    moar symbols

--- lib/special_character.cpp
+++ lib/special_character.cpp
@@ -3,8 +3,13 @@
 static CharacterMapping init()
 {
 	CharacterMapping characters;
-	characters[ "dots" ] = u8"\u2026";
-	characters[ "esc" ]  = "\033";
+
+	characters[ "dots" ]           = u8"\u2026";
+	characters[ "esc" ]            = u8"\033";
+	characters[ "lock" ]           = u8"\ue0a2";
+	characters[ "network" ]        = u8"\ue0a2";
+	characters[ "separator" ]      = u8"\ue0b0";
+	characters[ "separator_thin" ] = u8"\ue0b1";
 
 	return characters;
 }
