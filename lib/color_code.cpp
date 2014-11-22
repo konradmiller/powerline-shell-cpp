eefe33b896181329ab557afa2a189c028d6f2fbd
Konrad Miller <miller@kit.edu>
2014-11-22 14:26:57 +0100

    pass on all parameters from convenience function

--- lib/color_code.cpp
+++ lib/color_code.cpp
@@ -2,6 +2,6 @@
 
 std::string setColor( Color foreground, Color background, bool bright, ColorVariant variant )
 {
-	return ColorCode::getBasicColorCode( foreground, background );
+	return ColorCode::getBasicColorCode( foreground, background, bright, variant );
 }
 
