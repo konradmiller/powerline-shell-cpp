ee577598cabdf4bef8ab3524c68d879e2537ffaa
Konrad Miller <miller@kit.edu>
2014-11-24 13:24:40 +0100

    color chart with values for quick reference

--- lib/test_color.cpp
+++ lib/test_color.cpp
@@ -4,27 +4,36 @@
 std::string EnumerateColors()
 {
 	std::stringstream rainbow;
+
+	// legend for green boxes
+	for( unsigned short g = 0; g < 6; ++g )
+		rainbow << "          green = " << g << "        ";
+	rainbow << std::endl << std::endl;
+
+	// legend for blue values
+	rainbow << "   blue = ";
+	for( unsigned short g = 0; g < 6; ++g )
+	{
+		for( unsigned short b = 0; b < 6; ++b )
+			rainbow << " " << b << "  ";
+		rainbow << "   ";
+	}
+	rainbow << std::endl << std::endl;
+	
 	for( unsigned short r = 0; r < 6; ++r )
 	{
+		rainbow << " red = " << r << "  ";
 		for( unsigned short g = 0; g < 6; ++g )
 		{
 			for( unsigned short b = 0; b < 6; ++b )
-				rainbow << ColorCombination( Color(r,g,b), Color(r,g,b) ) << " x ";
+			{
+				rainbow << ColorCombination( Color(r,g,b), Color(r,g,b) ) << "  ";
+				rainbow << ColorCombination::resetColor() << "  ";
+			}
+			rainbow << "   ";
 		}
-		rainbow << ColorCombination::resetColor() << std::endl;
+		rainbow << ColorCombination::resetColor() << std::endl << std::endl;
 	}
-	rainbow << ColorCombination::resetColor() << std::endl << std::endl;
-
-	for( unsigned short r = 0; r < 6; ++r )
-		rainbow << ColorCombination( Color(r,0,0), Color(r,0,0) ) << " x ";
-
-	for( unsigned short g = 0; g < 6; ++g )
-		rainbow << ColorCombination( Color(0,g,0), Color(0,g,0) ) << " x ";
-	
-	for( unsigned short b = 0; b < 6; ++b )
-		rainbow << ColorCombination( Color(0,0,b), Color(0,0,b) ) << " x ";
-
-	rainbow << ColorCombination::resetColor() << std::endl << std::endl;
 
 	return rainbow.str();
 }
@@ -34,7 +43,18 @@ std::string EnumerateGrayscale()
 {
 	std::stringstream rainbow;
 	for( unsigned short g = 0; g < 24; ++g )
-		rainbow << ColorCombination( Color(g), Color(g) ) << " x ";
+	{
+		rainbow.width( 2 );
+		rainbow << std::right << g;
+		rainbow << "  ";
+	}
+	rainbow << std::endl;
+
+	for( unsigned short g = 0; g < 24; ++g )
+	{
+		rainbow << ColorCombination( Color(g), Color(g) ) << "  ";
+		rainbow << ColorCombination::resetColor() << "  ";
+	}
 
 	rainbow << ColorCombination::resetColor() << std::endl << std::endl;
 	
